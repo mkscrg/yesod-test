05659b0341f7e88613cb5b8a1b8c685b248e4216
Michael S. Craig <mkscrg@gmail.com>
2011-11-12 11:50:57 -0500

    Initial commit

---
+++ yesod-test.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE MultiParamTypeClasses, TypeFamilies
+           , QuasiQuotes, TemplateHaskell
+           , OverloadedStrings #-}
+
+import Yesod.Dispatch
+import Yesod.Core
+import Yesod.Content
+import Data.ByteString ( ByteString )
+import Network.Wai.Handler.Warp ( run )
+
+
+data Test = Test
+
+mkYesod "Test" [parseRoutes|
+/ TestR GET
+|]
+
+instance Yesod Test where
+    approot _ = ""
+    encryptKey _ = return Nothing
+
+getTestR :: Handler RepPlain
+getTestR = return . RepPlain $ toContent ("Test " :: ByteString )
+
+main = toWaiAppPlain Test >>= run 3000
